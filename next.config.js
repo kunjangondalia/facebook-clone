7ff1bfdbd91dec5fef11d30d83fe7c7d88acdd92
Kunjan <kunjan.gondalia.1@gmail.com>
2023-04-20 18:09:22 +0530

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{
+    domains: [
+      "upload.wikimedia.org",
+      "avatars.dicebear.com",
+      "images.pexels.com",
+      "platform-lookaside.fbsbx.com",
+      "static.xx.fbcdn.net",
+      "commons.wikimedia.org",
+      "www.pexels.com",
+      "cdn.pixabay.com"
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "inspector";
+import { Provider } from "react-redux";
+import store from "../public/src/app/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
+    );
 }
